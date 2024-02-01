35e003c6f89147c4d26084d90a5d7ad2c89a8160
Thanatip-iceJ <p.thanatip1@gmail.com>
2024-02-01 22:23:58 +0700

    added icon hover

--- src/components/IconItem.jsx
+++ src/components/IconItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function IconItem({ icon }) {
-  return <div>{icon}</div>;
+  return (
+    <div className="rounded-full hover:bg-slate-500/15 p-1.5 flex items-center cursor-pointer ">
+      {icon}
+    </div>
+  );
 }
 
 export default IconItem;
