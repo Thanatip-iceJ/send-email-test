e44e524aba26cf284a16f3fdb03af63928c9d569
Thanatip-iceJ <p.thanatip1@gmail.com>
2024-02-01 22:29:57 +0700

    added button hover and transitions

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button() {
     <div>
       <input
         type="submit"
-        className="font-semidbold bg-orange-500 font-semibold text-white py-2 w-full rounded-md cursor-pointer"
+        className="font-semidbold bg-orange-500 hover:bg-orange-500/80 font-semibold text-white py-2 w-full rounded-md cursor-pointer transition-all duration-150"
         value={"Send Email"}
       />
     </div>
--- src/components/IconItem.jsx
+++ src/components/IconItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function IconItem({ icon }) {
   return (
-    <div className="rounded-full hover:bg-slate-500/15 p-1.5 flex items-center cursor-pointer ">
+    <div className="rounded-full hover:bg-slate-500/15 p-1.5 flex items-center cursor-pointer transition-all duration-150 ">
       {icon}
     </div>
   );
